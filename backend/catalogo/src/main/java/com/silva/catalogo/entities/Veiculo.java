c6f96f194f268e4b476a6c69882d4a0d0bd9d899
Guilherme Ferreira <guilhermeferreira7930@gmail.com>
2022-05-30 10:07:11 -0300

    entities e classe veiculo

---
+++ backend/catalogo/src/main/java/com/silva/catalogo/entities/Veiculo.java
@@ -0,0 +1,79 @@
+package com.silva.catalogo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Veiculo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String marca;
+	private String modelo;
+	private String foto;
+	private Double preco;
+	
+	public Veiculo() {
+	}
+	
+	public Veiculo(Long id, String nome, String marca, String modelo, String foto, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.foto = foto;
+		this.preco = preco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
