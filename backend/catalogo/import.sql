d2d58f9bacb9979fc98c07ea1d2cb59c820518d3
Guilherme Ferreira <guilhermeferreira7930@gmail.com>
2022-05-31 19:42:40 -0300

    update dados

--- backend/catalogo/import.sql
+++ backend/catalogo/import.sql
@@ -7,3 +7,5 @@ INSERT INTO veiculo(marca, modelo, nome, preco, foto) VALUES("Bmw", "328i SPORT 
 INSERT INTO veiculo(marca, modelo, nome, preco, foto) VALUES("Suzuki", "Jimny 4ALL", "Suzuki Jimny 4ALL", 74.299, "https://images.kavak.services/images/130053/EXTERIOR-frontSidePilotNear-16367489553727.jpg?d=540x310");
 INSERT INTO veiculo(marca, modelo, nome, preco, foto) VALUES("Toyota", "Corolla ALTIS", "Toyota Corolla ALTIS", 94.999, "https://images.kavak.services/images/126960/EXTERIOR-frontSidePilotNear-16370909549766.jpg?d=540x310")
 INSERT INTO veiculo(marca, modelo, nome, preco, foto) VALUES("LAND ROVER", "DISCOVERY SPORT TD4 TURBO HSE", "LAND ROVER DISCOVERY SPORT TD4 TURBO HSE", 214.999, "https://images.kavak.services/images/112456/EXTERIOR-frontSidePilotNear-16315568921606.jpg?d=540x310");
+
+INSERT INTO usuario(username, password, authorities) values("guilherme", "123456", "ROLE_ADMIN,ROLE_USER");
